e422304aef6acf1ad96fde8a8094aaedf285d9d2
ksgalang1 <84303309+ksgalang1@users.noreply.github.com>
2022-06-05 08:50:51 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,22 @@
+<?php
+	include 'attendancefunctions.php';
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "attendancedb";
+	$conn = mysqli_connect($GLOBALS['servername'], $GLOBALS['username'], $GLOBALS['password'], $GLOBALS['dbname']);
+	
+	$sql = "SELECT * FROM users WHERE user_id LIKE '".$_SESSION['current_acc']."'";
+	$result = @mysqli_query($conn,$sql);
+	if (mysqli_num_rows($result) == 1){
+		header("Location: user.php");
+	}
+	$sql = "SELECT * FROM attendees WHERE attendee_id LIKE '".$_SESSION['current_acc']."'";
+	$result = @mysqli_query($conn,$sql);
+	if (mysqli_num_rows($result) == 1){
+		header("Location: attendee.php");
+	}
+?>
 <html>
 <head>
 <title>nffe login page</title>
@@ -43,7 +62,6 @@ $(document).ready(function(){
 </script>
 </head>
 <body style="background-image:url('https://images7.alphacoders.com/748/thumb-1920-748838.png');background-size:cover;overflow: hidden">
-
 <div style="margin-left:40%;margin-right:40%;margin-top:20%;padding:1%;border:solid;background-color:#FFFFFF;display:flex;justify-content:center;align-items:center;">
 <table>
 	<th style="justify-content:center;align-items:center;">
